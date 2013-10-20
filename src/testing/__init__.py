55e928606b7a94e68b5543e20a5dbf52348cc409
Richard <rl221@kent.ac.uk>
2013-10-20 13:41:05 +0100

    Implemented testing frame work. Pair programmed by Michael and Richard.
    Implemented a testing platform using the python module pytest. See wiki
    for instructions on installation and use.
    To execute, run the testing package.
    
    Created the src folder, all python module/packages will go in here.

---
+++ src/testing/__init__.py
@@ -0,0 +1,9 @@
+'''
+Created on 20 Oct 2013
+
+@author: richard
+'''
+
+import pytest
+
+pytest.main()
---
+++ src/testing/test_example.py
@@ -0,0 +1,14 @@
+'''
+Created on 20 Oct 2013
+
+@author: richard
+'''
+
+def increment(x):
+    return x + 1
+
+def test_increment():
+    assert increment(1) == 2
+
+# def test_failed_increment():
+#     assert increment(1) == 1
