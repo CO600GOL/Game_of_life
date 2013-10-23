dbab4f4d39d9a475d8fdea1d7ab394cf73d1e0df
Richard <rl221@kent.ac.uk>
2013-10-23 14:23:40 +0100

    Infrastructure in place for calculator implementation. Paired - Michael and Richard.

---
+++ src/game_of_life/engine/calculator.py
@@ -0,0 +1,16 @@
+'''
+Created on 23 Oct 2013
+
+@author: richard
+'''
+
+
+class Calculator(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
---
+++ src/testing/test_game_of_life/test_engine/test_calculator.py
@@ -0,0 +1,12 @@
+'''
+Created on 23 Oct 2013
+
+@author: richard
+'''
+
+
+class TestCalculator(object):
+    '''
+    classdocs
+    '''
+    pass
