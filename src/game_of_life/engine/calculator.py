c7eeab99d54f197ab1c2f72ed0f297148aa3e7ba
Richard and Michael <rl221@kent.ac.uk>
2013-10-24 14:54:08 +0100

    Add stub methods for calculator class. Paired by Michael and Richard

--- src/game_of_life/engine/calculator.py
+++ src/game_of_life/engine/calculator.py
@@ -1,16 +1,37 @@
 '''
 Created on 23 Oct 2013
 
-@author: richard
+@author: Richard and Michael
 '''
 
 
 class Calculator(object):
     '''
-    classdocs
+    This class represents the calculation engine for the Game of Life
+    simulation
     '''
 
-    def __init__(self):
+    def __init__(self, rule_set):
         '''
         Constructor
         '''
+        pass
+
+    def _find_neighbour_set(self, x, y):
+        '''
+        Returns a collection of a cells neighbours
+        '''
+        pass
+
+    def _next_state(self, cells):
+        '''
+        Returns the next state of the cell after calculation according to the
+        rule set.
+        '''
+        pass
+
+    def calculate_generation(self, grid):
+        '''
+        Calculates the next generation of cells in a given grid.
+        '''
+        pass
