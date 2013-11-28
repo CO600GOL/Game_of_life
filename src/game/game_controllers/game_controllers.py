3d56c3a51408d7287a002ce65c100a28df85753c
Michael <mw362@kent.ac.uk>
2013-11-28 21:47:34 +0000

    Wrote framework for GameOfLifeController. Programmed by Michael.
    Feature #44 - Stub methods for GameOfLifeController are now in place.
    Writing test framework is the next step.

--- src/game/game_controllers/game_controllers.py
+++ src/game/game_controllers/game_controllers.py
@@ -18,7 +18,7 @@ class GameController(object):
     all game controls.
     '''
 
-    def __init__(self, time=300):
+    def __init__(self, time):
         '''
         Ctor - Initialises game controller.
         '''
@@ -42,3 +42,30 @@ class GameController(object):
         Plays a single turn of the game.
         '''
         self._game.next_turn()
+
+
+class GameOfLifeController(GameController):
+    '''
+    This class represents the controller for the Game of Life.
+    This class will act as a bridge between the GoL game engine and
+    any output we create, controlling what access the output has
+    to the game engine.
+    '''
+
+    def __init__(self, time=300):
+        '''
+        Ctor - Initialises the GoL Controller.
+        '''
+        pass
+
+    def play_next_turn(self):
+        '''
+        Plays a single turn of the Game of Life.
+        '''
+        pass
+
+    def play_game(self):
+        '''
+        Plays the Game of Life until it ends.
+        '''
+        pass
