8bd046e164d55e91b96e7f18cb0e6facb4ac800a
Michael <mw362@kent.ac.uk>
2013-11-28 14:18:21 +0000

    Began writing tests. Programmed by Michael.
    I began writing tests for the GameController (Feature #38). Food for
    thought - if we abstract game controller, each type we create can
    initialise the game its playing without the game being given to it in
    the call. So 
    golc = golController() instead of
    gol = GameOfLife()
    golc = golController(gol)

--- src/testing/test_game/test_game_controllers/test_game_controllers.py
+++ src/testing/test_game/test_game_controllers/test_game_controllers.py
@@ -7,6 +7,9 @@ This module contains the automated tests for all the system's
 game controllers.
 '''
 
+from game.game_controllers.game_controllers import GameController
+from game.game import Game
+
 
 class TestGameController(object):
     '''
@@ -17,25 +20,40 @@ class TestGameController(object):
         '''
         Tests initialisation of a GameController.
         '''
-        pass
+        gc = GameController()
+        assert gc
 
     def test_set_game(self):
         '''
         Tests a game object can be set to the game controller.
         '''
-        pass
+        gc = GameController()
+        g = Game()
+
+        # test logic works
+        gc.set_game(g)
+        assert gc._game
+
+        # Test logic works as expected
+        assert gc._game == g
 
     def test_get_game(self):
         '''
         Tests the game currently being played can be retrieved.
         '''
-        pass
+        gc = GameController()
+        g = Game()
+        gc.set_game(g)
+
+        returned_game = gc.get_game()
+        assert returned_game == g
 
     def test_get_time_remaining(self):
         '''
         Tests the remaining time on the controller can be retrieved.
         '''
-        pass
+        gc = GameController(500)
+        assert gc.get_time_remaining() == 500
 
     def test_play_next_turn(self):
         '''
