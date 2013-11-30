b6c20e125f227755d2495ef80b7d3256a02019c6
Michael <mw362@kent.ac.uk>
2013-11-30 13:00:53 +0000

    GameOfLifeController completed. Paired by Michael and Geoff.
    We have completed the GameOfLifeController (Feature #44). It may need
    reviewing but what we have now works, and all related tests pass.

--- src/game/game_controllers/game_controllers.py
+++ src/game/game_controllers/game_controllers.py
@@ -8,6 +8,14 @@ by the system.
 '''
 
 from game.game import Game
+
+from game_of_life.data_structures.grid import GolGrid
+from game_of_life.data_structures.cell import GolCell
+from game_of_life.data_structures.states import Alive
+
+from game_of_life.engine.rule_sets import RuleSetStandard
+from game_of_life.engine.game_of_life import GameOfLife
+
 from utilities.timer import Timer
 
 
@@ -22,8 +30,13 @@ class GameController(object):
         '''
         Ctor - Initialises game controller.
         '''
-        self._game = Game()
         self._timer = Timer(time)
+
+    def set_up_game(self):
+        '''
+        Sets up the game to be played by this controller.
+        '''
+        self._game = Game()
 
     def get_game(self):
         '''
@@ -56,16 +69,47 @@ class GameOfLifeController(GameController):
         '''
         Ctor - Initialises the GoL Controller.
         '''
-        pass
+        GameController.__init__(self, time)
+
+    def set_up_game(self, init_input):
+        '''
+        Sets the initial input of the Game Of Life.
+        '''
+        GameController.set_up_game(self)
+
+        # Create the initial input
+        cell_pattern = []
+
+        for row in range(0, len(init_input)):
+            cell_pattern.append([])
+            for i in init_input[row]:
+                if i == 0:
+                    cell_pattern[row].append(GolCell())
+                else:
+                    cell_pattern[row].append(GolCell(Alive()))
+
+        initial_input = GolGrid(cell_pattern)
+
+        # Create the Rule Set
+        rule_set = RuleSetStandard()
+
+        # Set up game
+        self._game = GameOfLife(rule_set, initial_input)
 
     def play_next_turn(self):
         '''
         Plays a single turn of the Game of Life.
         '''
-        pass
+        if not self.get_game().is_game_forsaken():
+            self.get_game().next_turn()
 
     def play_game(self):
         '''
         Plays the Game of Life until it ends.
         '''
-        pass
+        self._timer.start()
+
+        while not self.get_game().is_game_forsaken() and\
+        self._timer.get_time_remaining() > 0:
+            self.get_game().next_turn()
+        self._timer.stop()
