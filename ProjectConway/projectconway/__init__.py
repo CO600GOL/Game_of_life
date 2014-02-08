a19f8a870956450e7ea483db8210d7489f2f2384
Richard Lancaster <rl221@kent.ac.uk>
2014-02-08 13:49:49 +0000

    Renamed "pattern_input" to "create"
    This is in preparation of combining the create pages into one view.
    Paired by Niklas and Richard

--- ProjectConway/projectconway/__init__.py
+++ ProjectConway/projectconway/__init__.py
@@ -15,7 +15,7 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('pattern_input', '/create')
+    config.add_route('create', '/create')
     config.add_route('pattern_input_receiver', '/pattern_receiver.json')
     config.add_route('pattern_input_clearer', '/pattern_clearer.json')
     config.add_route('about', '/about')
