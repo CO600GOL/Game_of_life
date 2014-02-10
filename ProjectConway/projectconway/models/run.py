a6b40d5587ea6476f0740ea7d27f00ad9bff1f31
Richard Lancaster <rl221@kent.ac.uk>
2014-02-10 19:48:19 +0000

    Massively improve the time slot query

--- ProjectConway/projectconway/models/run.py
+++ ProjectConway/projectconway/models/run.py
@@ -10,12 +10,12 @@ class Run(Base):
     Game Of Life will be held.
     '''
     __tablename__ = "runs"
-    
+
     id = Column(Integer, Sequence('run_id_seq'), primary_key=True)
     input_pattern = Column(String)
     time_slot = Column(DateTime)
     user_name = Column(String(50))
-    
+
     def __init__(self, input_pattern, time_slot, user_name):
         '''
         Initialises an object to be input into
@@ -24,12 +24,12 @@ class Run(Base):
         self.input_pattern = input_pattern
         self.time_slot = time_slot
         self.user_name = user_name
-        
+
     def __repr__(self):
         '''
         Returns: a representation of the table object.
         '''
-        return("Run<Pattern=%s, Time Slot=%s, User Name=%s>" % (self.input_pattern, self.time_slot, self.user_name))
+        return ("Run<Pattern=%s, Time Slot=%s, User Name=%s>" % (self.input_pattern, self.time_slot, self.user_name))
 
     @classmethod
     def get_time_slots_match_hour(cls, time_slot):
@@ -37,9 +37,10 @@ class Run(Base):
         Returns the times slots for the same year, month, day, hour as a
         given datetime object
         """
+        min_hour = time_slot.replace(minute=0, second=0, microsecond=0)
+        max_hour = time_slot.replace(minute=0, second=0, microsecond=0) + datetime.timedelta(hours=1)
 
         return DBSession.query(Run.time_slot).filter(and_(
-            extract("year", Run.time_slot) == time_slot.year,
-            extract("month", Run.time_slot) == time_slot.month,
-            extract("day", Run.time_slot) == time_slot.day,
-            extract("hour", Run.time_slot) == time_slot.hour)).all()
+            Run.time_slot <= max_hour,
+            Run.time_slot >= min_hour
+        )).all()
