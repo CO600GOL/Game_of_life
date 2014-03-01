b6cb68b5522cd8675961e9b3898b3ddd0c4c1ebe
Richard Lancaster <rl221@kent.ac.uk>
2014-03-01 16:35:49 +0000

    Another small patch to the install script of DisplayAdapter
    Paired by Richard and Michael

--- DisplayAdapter/setup.py
+++ DisplayAdapter/setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
 requires = [
-    "display_adapter",
     "pytest"
+]
+
+packages = [
+    "display_adapter"
 ]
 
 setup(
     name='DisplayAdapter',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages() + packages,
     install_requires=requires,
     license='MIT',
 )
