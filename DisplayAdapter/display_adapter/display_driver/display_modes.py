1fec85ee94404a4d84e2479e240a5fe670669ef5
Richard Lancaster <rl221@kent.ac.uk>
2014-03-01 20:49:49 +0000

    Wrote skeleton framework for RunMode, ScreensaverMode and testing classes. Paired by Richard and Michael.
    The classes for the above classes have been written but are currently unimplemented. (Feature #175)

--- DisplayAdapter/display_adapter/display_driver/display_modes.py
+++ DisplayAdapter/display_adapter/display_driver/display_modes.py
@@ -46,3 +46,58 @@ class DisplayMode(object):
         temp = DisplayMode._game_engine.get_current_generation(output=True)
         DisplayMode._game_engine.play_next_turn()
         return temp
+
+
+class ModeRun(DisplayMode):
+    """
+    This class represents the mode in which the display runs when it is showing a user's input pattern.
+    """
+
+    def __init__(self, pattern):
+        """
+        Ctor - initialises the Run mode with the correct information. Main function is
+        to set up the game engine used for the specified pattern.
+        """
+        pass
+
+    def is_active(self):
+        """
+        This method returns whether or not it has any more patterns to pass back to the display driver.
+        """
+        pass
+
+    def get_display_pattern(self):
+        """
+        This method returns the next pattern to be displayed as a string. Usually this will be the next pattern
+        from the game engine, but if on start up, it will enter a 'presentation state' to let the user know that
+        their pattern is being run.
+        """
+        pass
+
+
+class ScreensaverMode(DisplayMode):
+    """
+    This class represents the mode in which the display runs when it is not showing a user's input pattern.
+    """
+    pass
+
+    def __init__(self, pattern):
+        """
+        Ctor - initialises the Screensaver mode with the correct information. Main function is
+        to set up the game engine used for the specified pattern.
+        """
+        pass
+
+    def is_active(self):
+        """
+        This method returns whether or not the mode has any more patterns to pass back to the display driver.
+        """
+        pass
+
+    def get_display_pattern(self):
+        """
+        This method returns the next pattern to be displayed as a string. Usually this will be the next pattern from
+        the game engine, but if on start up, it will enter a 'clear state' where it wipes whatever was on the display
+        away to let the users know it is entering screensaver mode.
+        """
+        pass
--- DisplayAdapter/testing/test_display_adapter/test_display_driver/test_display_modes.py
+++ DisplayAdapter/testing/test_display_adapter/test_display_driver/test_display_modes.py
@@ -49,3 +49,72 @@ class TestDisplayMode(object):
 
         assert dm.get_display_pattern() == "-*-\n-*-\n-*-"
         assert dm.get_display_pattern() == "***\n***\n***"
+
+
+class TestRunMode(object):
+    """
+    This class tests the display's Run mode in order to test the working logic of the display when showing a player's
+    pattern.
+    """
+
+    def test_init(self):
+        """
+        Tests initialisation of the display's Run mode.
+        """
+        pass
+
+    def test_is_active(self):
+        """
+        Ensuring that the run is still active, especially in it's presentation state.
+        """
+        pass
+
+    def test_is_active_fails(self):
+        """
+        Ensuring that the run is not active after the mode leaves it's presentation state.
+        """
+        pass
+
+    def test_get_display_pattern(self):
+        """
+        Ensuring that the mode is, under all circumstances, returning the correct pattern to be displayed.
+        The circumstances are as follows:
+            - Two patterns during presentation state; all lights on and pattern lights on.
+            - Once presentation state has finished, the player's pattern at the beginning of their cycle.
+        """
+        pass
+
+
+class TestScreensaverMode(object):
+    """
+    This class tests the display's Screensaver mode in order to test the working logic of the display when not showing
+    a player's pattern.
+    """
+
+    def test_init(self):
+        """
+        Tests initialisation of the display's Run mode.
+        """
+        pass
+
+    def test_is_active(self):
+        """
+        Ensuring that the run is still active, especially in it's clear state.
+        """
+        pass
+
+    def test_is_active_fails(self):
+        """
+        Ensuring that the run is not active after the mode leaves it's clear state.
+        """
+        pass
+
+    def test_get_display_pattern(self):
+        """
+        Ensuring that the mode is, under all circumstances, returning the correct pattern to be displayed.
+        The circumstances are as follows:
+            - A pause of the frame on the display when it entered screensaver mode.
+            - The correct number of clear frames when running in clear state.
+            - Once clear state has finished, the correct frames for a screensaver pattern.
+        """
+        pass
