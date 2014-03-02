0ccbc07e6f4fdab63428f6d92f0f22dae03b80f7
Richard Lancaster <rl221@kent.ac.uk>
2014-03-02 01:01:22 +0000

    The skeleton and testing framework for the Display Driver has been implemented. Programmed by Richard and Michael
    The skeleton framework for the display driver, as well its testing platform, have been created, but
    so far no implementation has been written. (Feature #173)

---
+++ DisplayAdapter/display_adapter/display_driver/display_drivers.py
@@ -0,0 +1,25 @@
+"""
+This module contains the functionality of all device drivers, which serve as the highest-level controllers of the
+device within the raspberry pi. Though they maintain control of the data being passed to the display, they do not do
+this directly, instead handing the data off to another helper class.
+"""
+
+class DisplayDriver(object):
+    """
+    This class represents the display driver that links the raspberry pi to the display. This driver will have high-
+    level control over the data being sent to the display, but will do this mostly through the help of smaller utility
+    classes.
+    """
+
+    def __init__(self):
+        """
+        Ctor - Initialises the display driver, setting up the database helper and display controller.
+        """
+        pass
+
+    def start(self):
+        """
+        Starts the application: begins checking the current time and working out what information needs to be sent
+        to the display at that time.
+        """
+        pass
---
+++ DisplayAdapter/testing/test_display_adapter/test_display_driver/test_display_drivers.py
@@ -0,0 +1,17 @@
+"""
+This module contains the testing framework for the display driver functionality, and is responsible for testing whether
+the pi can correctly and sufficiently connect to the display.
+"""
+
+class TestDisplayDriver(object):
+    """
+    This class tests the functionality of the DisplayDriver class, ensuring that it can correctly connect the raspberry
+    pi to the display.
+    """
+
+    def test_init(self):
+        """
+        This method tests initialisation of the display driver, ensuring it has correctly set up the database helper
+        and the display controller.
+        """
+        pass
