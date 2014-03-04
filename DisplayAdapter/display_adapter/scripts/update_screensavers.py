0808821d2251659e89f86f2b9eb22e255c2ddd2d
Richard Lancaster <rl221@kent.ac.uk>
2014-03-04 20:33:30 +0000

    Script for updating screensavers table complete. Paired by Richard and Michael.
    The script for updating the internal database's screensavers table has been written and according to tests, works. (Feature #
    180).

--- DisplayAdapter/display_adapter/scripts/update_screensavers.py
+++ DisplayAdapter/display_adapter/scripts/update_screensavers.py
@@ -2,3 +2,27 @@
 This script pulls all available screensavers from the screensavers.txt file and
 puts them in the raspberry pi's internal database.
 """
+
+import sqlite3
+from display_adapter import db_name
+
+if __name__ == "__main__":
+    with open("display_adapter/scripts/screensavers.txt", "r") as file:
+        screensavers = file.read()
+        screensavers = screensavers.split("\n\n")
+
+        if screensavers:
+            con = sqlite3.connect(db_name)
+
+            for screensaver in screensavers:
+                statement = """
+INSERT INTO screensavers
+    SELECT "%s"
+    WHERE NOT EXISTS (SELECT * FROM screensavers WHERE pattern = "%s")
+""" % (screensaver, screensaver)
+                con.execute(statement)
+
+            con.commit()
+            con.close()
+
+
