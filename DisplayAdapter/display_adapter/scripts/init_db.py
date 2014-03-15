bff3cbc3133a83f21cd3640ed431e29fcd774cf7
Michael Wilson <soothsayre@hotmail.co.uk>
2014-03-15 12:08:06 +0000

    Code documentation added. Written by Michael.
    Added documentation to some of the display_adapter scripts. (Support #205)

--- DisplayAdapter/display_adapter/scripts/init_db.py
+++ DisplayAdapter/display_adapter/scripts/init_db.py
@@ -1,5 +1,5 @@
 """
-Script that is run from the command line in order to
+This python script initialises the raspberry pi's internal database.
 """
 import sys
 import sqlite3
@@ -11,6 +11,7 @@ This initialises an sqlite3 db for the purposes of the DisplayAdapter programs.
 Arguments: init_db.py database_name
 """
 
+# SQLite 3 table in which to hold the users' patterns and the time at which they are to be run.
 runs_table = """
 CREATE TABLE runs (
     id INTEGER NOT NULL,
@@ -21,6 +22,7 @@ CREATE TABLE runs (
 )
 """
 
+# SQLite 3 table in which to hold the screensaver patterns played when the display is not showing a user's patterns
 screensavers_table = """
 CREATE TABLE screensavers (
     pattern VARCHAR
@@ -30,22 +32,26 @@ CREATE TABLE screensavers (
 
 def init_db(db_name=db_name):
     """
-    This function takes a database name and creates the database required
-    for the DisplayAdapter programs
+    This function takes a database name and creates the database required for the DisplayAdapter programs.
+
+    @param db_name the name of the database with which the applications should connect.
     """
     con = sqlite3.connect(db_name)
     cur = con.cursor()
 
     try:
+        # Attempt to set up the database
         cur.execute("pragma journal_mode=wal")
         cur.execute(runs_table)
         cur.execute(screensavers_table)
         con.commit()
     except sqlite3.OperationalError:
-        pass # We don't care if the table already excists
+        # If the code drops into this except, then one of the tables already exists.
+        pass
     con.close()
 
 
+# Run this script if the module is called from the command line
 if __name__ == "__main__":
     if len(sys.argv) > 2:
         if sys.argv[1].lower() == "help":
--- DisplayAdapter/display_adapter/scripts/update_screensavers.py
+++ DisplayAdapter/display_adapter/scripts/update_screensavers.py
@@ -1,6 +1,6 @@
 """
-This script pulls all available screensavers from the screensavers.txt file and
-puts them in the raspberry pi's internal database.
+This script pulls all available screensavers from the screensavers.txt file and puts them in the raspberry pi's
+internal database.
 """
 
 import json
@@ -9,15 +9,18 @@ from display_adapter import db_name
 
 current_file = "display_adapter/scripts/screensavers.txt"
 
+# Run this script if it is called from the command line.
 if __name__ == "__main__":
     with open(current_file, "r") as file:
         screensavers = json.loads(file.read())
 
         if screensavers:
+            # Connect to the raspberry pi's internal database.
             con = sqlite3.connect(db_name)
 
             for screensaver in screensavers:
                 screensaver = "\n".join(screensaver)
+                #SQLite 3 statement for writing screensaver's to the internal database.
                 statement = """
 INSERT INTO screensavers
     SELECT "%s"
@@ -25,6 +28,7 @@ INSERT INTO screensavers
 """ % (screensaver, screensaver)
                 con.execute(statement)
 
+            # Commit the changes and close the database connection.
             con.commit()
             con.close()
 
