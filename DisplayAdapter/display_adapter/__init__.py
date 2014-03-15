49b0dcb12c37f22c46129020da7ad6f1c94af704
Michael Wilson <soothsayre@hotmail.co.uk>
2014-03-15 13:05:05 +0000

    Coding documentation added. Written by Michael.
    Documentation for the test_database_receivers module has been added (Support #205)

--- DisplayAdapter/display_adapter/__init__.py
+++ DisplayAdapter/display_adapter/__init__.py
@@ -1,10 +1,14 @@
-__author__ = 'richard'
+"""
+This __init__ file holds some centralised information for the display_adapter library.
+"""
 
+# Database information
 db_name = "internal_db.sqlite"
 test_db_name = "test_internal_db.sqlite"
 db_receiver_url = "http://127.0.0.1:6543/run_transmitter.json"
 db_receiver_polling_period = 1 # minutes
 
+# Serial information
 serial_name = "/dev/ttyACM0"
 serial_baudrate = 9600
 sleep_time = 0.5 # Amount of time the display sleeps between generations
