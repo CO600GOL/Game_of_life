6e8af5ae402be41b4a39b3071620db7894beba45
Michael Wilson <soothsayre@hotmail.co.uk>
2014-03-15 15:27:34 +0000

    Code documentation added. Written by Michael
    Documentation for the test_display_drivers module has been completed. (Support #205)

--- DisplayAdapter/testing/test_display_adapter/test_display_driver/test_display_drivers.py
+++ DisplayAdapter/testing/test_display_adapter/test_display_driver/test_display_drivers.py
@@ -1,14 +1,14 @@
 """
-This module contains the testing framework for the display driver functionality, and is responsible for testing whether
-the pi can correctly and sufficiently connect to the display.
+This module contains testing logic for the display driver. The tests should make sure the driver can correctly set up
+the necessary helper classes.
 """
 
 from display_adapter.display_driver.display_drivers import DisplayDriver
 
 class TestDisplayDriver(object):
     """
-    This class tests the functionality of the DisplayDriver class, ensuring that it can correctly connect the raspberry
-    pi to the display.
+    This class tests the functionality of the DisplayDriver class, ensuring that it can correctly connect the Raspberry
+    Pi to the display.
     """
 
     def test_init(self):
@@ -18,6 +18,9 @@ class TestDisplayDriver(object):
         """
         dd = DisplayDriver()
 
+        # Assert that the DisplayDriver exisrs
         assert dd
+        # Assert that the display driver's database helper has been correctly set
         assert hasattr(dd, "_db_helper")
+        # Assert that the display driver's display controller has been correctly set
         assert hasattr(dd, "_display_controller")
