13b458ff359690035cf2e7cb72bbed376a0fe533
Michael Wilson <soothsayre@hotmail.co.uk>
2014-03-15 10:56:15 +0000

    Code documentation added. Written by Michael.
    Documentation for the display_modes module has been written. (Support #205)

--- DisplayAdapter/display_adapter/__init__.py
+++ DisplayAdapter/display_adapter/__init__.py
@@ -12,7 +12,7 @@ sleep_time = 0.5 # Amount of time the display sleeps between generations
 # Information that is specific to the run mode animation
 runmode_config = {
     "iterations": 3, # Number of iterations of "full" and "pattern" frames in the animation
-    "full_frames": 1, # Number of "full" frames per animation interation
+    "full_frames": 1, # Number of "full" frames per animation iteration
     "pattern_frames": 2 # Number of "pattern" frames per animation iteration
 }
 
