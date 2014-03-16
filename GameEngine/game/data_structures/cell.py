f001ecae5174debf7ba424d5e3ec7a18a79c7a0e
Michael Wilson <soothsayre@hotmail.co.uk>
2014-03-16 15:54:24 +0000

    Code documentation added. Written by Michael
    Documentation for the cell, grid and states modules have been written, which means
    the documentation for all game/datastructures modules is complete (Support #205)

--- GameEngine/game/data_structures/cell.py
+++ GameEngine/game/data_structures/cell.py
@@ -1,40 +1,37 @@
-'''
-Created on 20 Oct 2013
-
-@author: Richard, Michael & Geoff.
-
-This is a module containing the Cell class.
-'''
+"""
+This module contains the logic representing board cells. It controls the state of an individual cell, storing any
+data important to that state.
+"""
 
 from game.data_structures.states import State
 
 
 class Cell(object):
-    '''
-    Cell represents a single cell on a Conway's Game of Life board. It can be
-    either alive or dead at an one time.
-    '''
+    """
+    This class represents a single cell on a game board. A cell has a state, which controls whether the light is on,
+    and possibly in the future, things such as the colour and intensity of the light.
+    """
 
     def __init__(self, state=State()):
-        '''
-        Sets the initial state of the cell to dead.
+        """
+        Ctor - initialises the cell and its state.
 
-        Keyword arguments:
-        state -- Initial state of cell. Defaults to Dead
-        '''
+        @param state The initial state of the cell. Defaults to 'dead' for the purposes of the Game of Life.
+        """
         self._state = state
 
     def get_state(self):
         """
-        Returns the current state of the cell
+        This method retrieves the current state of the cell.
+
+        @return the current state of the cell.
         """
         return self._state
 
     def set_state(self, state):
         """
-        Assign new state to cell object.
+        This method assigns a new state to the cell.
 
-        Arguments:
-        state -- new state to assign
+        @param state the new state with which to assign the cell.
         """
         self._state = state
--- GameEngine/game/data_structures/states.py
+++ GameEngine/game/data_structures/states.py
@@ -1,37 +1,33 @@
 '''
-Created on 20 Oct 2013
-
-@author: Richard and Michael
-
-This is a module containing the State class and all classes derived from it.
+This module contains the logic representing the cell states available during a game. A state may hold information about
+what the cell it belongs to should do while remaining in that state.
 '''
 
 
 class State(object):
     """
-    This class represents the state of a cell at any given time.
-    NOTE: This class is abstract.
+    This abstract class represents the state of a cell at any given time. It acts as a backbone for all states,
+    implementing all shared functionality.
     """
 
     def __init__(self):
         """
-        Ctor (Constructor)
-
-        Initialises a state, readying all state data for use by the game
-        engine. (Placeholder)
+        Ctor - Initialises a state, storing all state data for use.
         """
         pass
 
     def __eq__(self, other):
         '''
-        Allow boolean equality comparison of state object.
-        Returns true if objects are the same class.
+        This method overrides the object.__eq__ method to allow boolean equality comparison for state objects.
+
+        @return True If two objects are of the same class.
         '''
         return isinstance(other, self.__class__)
 
     def __ne__(self, other):
         '''
-        Allow boolean equality comparison of state object.
-        Returns true if two objects are not the same class.
+        This method overrides the object.__ne__ method to allow boolean equality comparison for state objects.
+
+        @return True if two objects are not of the same class.
         '''
         return not isinstance(other, self.__class__)
