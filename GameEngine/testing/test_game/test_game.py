dd71f7e9e396a817e041b50ccf4d3934ac11a20f
Michael Wilson <soothsayre@hotmail.co.uk>
2014-03-17 20:14:57 +0000

    Code documentation added. Written by Michael.
    Documentation for the test modules of the generic game engine, as well as the generic game controller
    and the GameOfLifeController has been written (Support #205)

--- GameEngine/testing/test_game/test_game.py
+++ GameEngine/testing/test_game/test_game.py
@@ -1,47 +1,49 @@
-'''
-Created on 27.11.2013
-
-@author: Michael and Richard
-
-This module contains the logic for testing the game
-module.
-'''
+"""
+This module contains logic that tests the generic game engine. These tests must evaluate whether a game engine can
+be correctly initialised and can function correctly.
+"""
 
 from game.game import Game
 
 
 class TestGame(object):
-    '''
-    This class acts to represent automated tests for the
-    Game class.
-    '''
+    """
+    This class tests the functionality of the Game class.
+    """
 
     def test_init(self):
-        '''
+        """
         Tests the initialisation of a Game object.
-        '''
+
+        This method tests the initialisation of a Game object. The expected result of this test is for a Game to be
+        correctly initialised.
+        """
         g = Game()
 
-        # Assert logic works
+        # Assert a game has been initialised.
         assert g
 
-        # Assert logic works as expected
+        # Assert the game has been initialised with the correct number of turns (will always be zero).
         assert g._turn_count == 0
 
     def test_next_turn(self):
-        '''
-        Tests whether the Game.next_turn() method functions correctly.
-        '''
+        """
+        This method tests the ability of the game engine to play its next turn. The expected result of this test is for
+        the next game turn to be played correctly.
+        """
         g = Game()
 
         g.next_turn()
+        # If the next turn has been played, the turn count will be 1 at this point. Assert this is true.
         assert g._turn_count == 1
 
     def test_get_turn_count(self):
-        '''
-        Tests whether the get_turn_count() method functions correctly.
-        '''
+        """
+        This method tests the ability of the game engine to retrieve the number of turns that have so far been played.
+        The expected result of this test is for the turn count to be correct at all times.
+        """
         g = Game()
         g.next_turn()
 
+        # Assert that after the next turn has been played, the retrieved turn count is correct.
         assert g.get_turn_count() == 1
