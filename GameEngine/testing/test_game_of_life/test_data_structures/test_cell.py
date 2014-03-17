e2f14ffef13f4145684f48e8020bc491091424f7
Michael Wilson <soothsayre@hotmail.co.uk>
2014-03-17 20:42:15 +0000

    Code documentation added. Written by Michael.
    Documentation for the testing logic of  Game of Life data structures (namely the GoLCell,
    GoLGrid and Alive and Dead states) has been written (Support #205)

--- GameEngine/testing/test_game_of_life/test_data_structures/test_cell.py
+++ GameEngine/testing/test_game_of_life/test_data_structures/test_cell.py
@@ -1,60 +1,66 @@
-'''
-Created on 20 Oct 2013
-
-@author: Michael and Richard
-'''
+"""
+This module contains logic that tests the data structure representing a Game of Life board cell. These tests must
+evaluate whether a cell can be correctly initialised and can function correctly.
+"""
 
 from game_of_life.data_structures.cell import GolCell
 from game_of_life.data_structures.states import Alive, Dead
 
 
 def initialise_cell():
-    '''
-    Abstracted initialisation as it was being repeated
-    '''
+    """
+    This function abstracts the initialisation of a GoLCell, to stop unnecessary repeated code.
+    """
     return GolCell()
 
 
 class TestCell(object):
-    '''
-    Tests the functionality of the cell module
-    '''
+    """
+    This class tests the functionality of the GoLCell class.
+    """
 
     def test_cell_init(self):
-        '''
-        Test initialisation of Cell object
-        '''
+        """
+        This method tests the initialisation of a GoLCell. The expected result of this test is for the cell to be
+        correctly initialised.
+        """
+        # Assert that the cell has been initialised.
         assert initialise_cell()
 
     def test_get_state(self):
-        '''
-        Test correct functionality of Cell's accessor method
-        '''
+        """
+        This method tests the ability of the GoLCell to retrieve its current state. The expected result of this test
+        is for the cell's current state to be correctly retrieved.
+        """
         c = initialise_cell()
         s = c.get_state()
-        # Make sure it returned a state
+        # Assert that the cell's state has been retrieved.
         assert s
-        # Make sure the state was Dead
+        # Assert that the cell's state is correct (at this point it will be Dead).
         assert isinstance(s, Dead)
 
     def test_is_alive(self):
-        '''
-        Test whether the cell knows it is alive.
-        '''
+        """
+        This method tests the ability of the GolCell to evaluate whether or not it is alive. The expected result of this
+        test is for the evaluation to be successful.
+        """
         c = GolCell(Alive())
+        # Assert that the GolCell can evaluate itself as alive.
         assert c.is_alive()
 
         c = initialise_cell()
+        # Assert that the GolCell can evaulate itself as dead.
         assert not c.is_alive()
 
     def test_set_state(self):
-        '''
-        Test correct functionality of Cell's mutator method
-        '''
+        """
+        This method tests the ability of the GolCell to set its new state. The expected result of this test is for a
+        new state to be correctly set.
+        """
         c = initialise_cell()
         c.set_state(Alive())
         s = c.get_state()
-        # Ensure that a state was set correct
+        # Assert that the state has been set.
         assert s
-        # Ensure that state was set as alive
+        # Assert that the state has been set to Alive.
         assert isinstance(s, Alive)
--- GameEngine/testing/test_game_of_life/test_data_structures/test_grid.py
+++ GameEngine/testing/test_game_of_life/test_data_structures/test_grid.py
@@ -1,10 +1,7 @@
-'''
-Created on 23 Oct 2013
-
-@author: Richard and Michael
-
-A module for testing the grid module.
-'''
+"""
+This module contains logic to test the data structure representing a Game of Life grid board. These tests should
+evaluate whether the GoLGrid can be initialised correctly and can function correctly.
+"""
 
 from game_of_life.data_structures.grid import GolGrid
 from game_of_life.data_structures.cell import GolCell
@@ -12,15 +9,15 @@ from game_of_life.data_structures.states import Alive, Dead
 
 
 class TestGrid(object):
-    '''
-    Tests the functionality of the grid module.
-    '''
+    """
+    This class tests the functionality of the GoLGrid module.
+    """
 
     def test_grid_init(self):
-        '''
-        Tests correct initialisation of grid objects. Objects should initialise
-        with a number of GolCells that are all dead.
-        '''
+        """
+        This method tests the initialisation of a GoLGrid object. The expected result of this test is that the GolGrid
+        should be initialised with a number of GoLCells, which are all dead.
+        """
         pattern = [[GolCell(),
                     GolCell(),
                     GolCell(),
@@ -39,27 +36,33 @@ class TestGrid(object):
                     GolCell()]]
 
         test_grid = GolGrid(pattern)
+        # Assert that the GoLGrid has been initialised.
         assert test_grid
 
         cells = test_grid.get_cells()
 
         for row in cells:
+            # For each row of cells.
             for c in row:
+                # Assert that the state of each cell is dead, as it should be.
                 assert isinstance(c.get_state(), Dead)
 
     def test_get_cells(self):
-        '''
-        Tests the ability for the grid to return its collection of GolCells.
-        '''
+        """
+        This method tests the ability of the GoLGrid to retrieve all of its GoLCells. The expected result of this
+        test is that the GoLCells are correctly retrieved.
+        """
         test_grid = GolGrid()
 
         cells = test_grid.get_cells()
+        # Assert that a collection of cells has been correctly retrieved.
         assert cells
 
     def test_set_cells(self):
-        '''
-        Tests the ability of the grid to store a collection of GolCells.
-        '''
+        """
+        This method tests the ability of the GoLGrid to store a collection of GoLCells. The expected result of this
+        tests is that a collection of GoLCells can be correctly set.
+        """
         test_grid = GolGrid()
 
         # Create a collection of GolCell objects
@@ -75,24 +78,29 @@ class TestGrid(object):
                     c.set_state(Alive())
                 cells[x].append(c)
 
-        # Write a test to check that the collection has been stored
         test_grid.set_cells(cells)
+        # Assert that the collection of GoLCells has been set correctly.
         assert test_grid.get_cells()
 
-        # Write a test to check that the correct collection has been stored
         recieved_cells = test_grid.get_cells()
         for x, row in enumerate(recieved_cells):
+            # For each row of cells.
             for y, _column in enumerate(row):
+                # Assert that the GoLCell at these 'coordinates' is the correct GoLCell.
                 assert recieved_cells[x][y] == cells[x][y]
 
     def test_get_no_alive_cells(self):
-        '''
+        """
         Tests the ability of the grid to retrieve the number of living cells.
-        '''
+
+        This method tests the ability of the GoLGrid to retrieve the number of living GoLCells it currently has.
+        """
         test_grid = GolGrid()
         alive_cells = test_grid.get_no_alive_cells()
+        # At this point, none of the GoLCells should be alive. Assert this is true.
         assert alive_cells == 0
 
+        # Create another set of cells, half of which are alive.
         s = 10
         cells = []
         for x in range(0, s):
@@ -100,9 +108,11 @@ class TestGrid(object):
             for y in range(0, s):
                 c = GolCell()
                 if y % 2 == 0:
+                    # Make every second cell alive.
                     c.set_state(Alive())
                 cells[x].append(c)
 
         test_grid = GolGrid(cells)
         alive_cells = test_grid.get_no_alive_cells()
+        # Assert that the correct number of cells have been retrieved.
         assert alive_cells == (s * s) / 2
