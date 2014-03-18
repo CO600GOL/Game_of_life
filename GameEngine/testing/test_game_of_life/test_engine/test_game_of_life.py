3e9c174e85580823aa39bd34957c01145b69e9e6
Michael Wilson <soothsayre@hotmail.co.uk>
2014-03-18 08:57:09 +0000

    Code documentation added. Written by Michael.
    Documentation for the testing logic of the Game of Life engine has been written. (Support #205)

--- GameEngine/testing/test_game_of_life/test_engine/test_game_of_life.py
+++ GameEngine/testing/test_game_of_life/test_engine/test_game_of_life.py
@@ -1,89 +1,93 @@
-'''
-Created on 28.10.2013
-
-@author: Michael and Geoff
-
-This module contains and runs all the tests for the game_of_life module.
-'''
+"""
+This module contains logic for testing the Game of Life engine. These tests must evaluate whether the game engine can
+be correctly initialised and can function correctly.
+"""
 
 from game_of_life.data_structures.grid import GolGrid
 from game_of_life.data_structures.cell import GolCell
 from game_of_life.data_structures.states import Alive
 from game_of_life.engine import game_of_life, rule_sets
 
+def set_current_generation(self):
+    """
+    This function creates a pattern of cells to use as the game engine's 'current generation'.
+
+    @return cells The collection of GoLCells to represent the current generation.
+    """
+    cells = [[GolCell(),
+              GolCell(),
+              GolCell(Alive()),
+              GolCell()],
+             [GolCell(Alive()),
+              GolCell(Alive()),
+              GolCell(Alive()),
+              GolCell()],
+             [GolCell(),
+              GolCell(),
+              GolCell(),
+              GolCell()],
+             [GolCell(Alive()),
+              GolCell(),
+              GolCell(Alive()),
+              GolCell()]]
+
+    return cells
+
+
+def set_next_generation(self):
+    """
+    This method creates a pattern of cells to use as the game engine's 'next generation'.
+
+    @return cells The collection of GoLCells to represent the next generation.
+    """
+
+    cells = [[GolCell(Alive()),
+              GolCell(),
+              GolCell(Alive()),
+              GolCell()],
+             [GolCell(),
+              GolCell(Alive()),
+              GolCell(Alive()),
+              GolCell(Alive())],
+             [GolCell(Alive()),
+              GolCell(),
+              GolCell(Alive()),
+              GolCell()],
+             [GolCell(),
+              GolCell(Alive()),
+              GolCell(),
+              GolCell(Alive())]]
+
+    return cells
+
 
 class TestGameOfLife(object):
-    '''
-    This class contains and runs the tests for the GameOfLife class in the
-    game_of_life module.
-    '''
-
-    def set_current_generation(self):
-        '''
-        Returns a pattern of cells to use as the 'current generation'
-        in testing.
-        '''
-        cells = [[GolCell(),
-                  GolCell(),
-                  GolCell(Alive()),
-                  GolCell()],
-                 [GolCell(Alive()),
-                  GolCell(Alive()),
-                  GolCell(Alive()),
-                  GolCell()],
-                 [GolCell(),
-                  GolCell(),
-                  GolCell(),
-                  GolCell()],
-                 [GolCell(Alive()),
-                  GolCell(),
-                  GolCell(Alive()),
-                  GolCell()]]
-
-        return cells
-
-    def set_next_generation(self):
-        '''
-        Returns a pattern of cells to use as the 'next generation'
-        in testing.
-        '''
-
-        cells = [[GolCell(Alive()),
-                  GolCell(),
-                  GolCell(Alive()),
-                  GolCell()],
-                 [GolCell(),
-                  GolCell(Alive()),
-                  GolCell(Alive()),
-                  GolCell(Alive())],
-                 [GolCell(Alive()),
-                  GolCell(),
-                  GolCell(Alive()),
-                  GolCell()],
-                 [GolCell(),
-                  GolCell(Alive()),
-                  GolCell(),
-                  GolCell(Alive())]]
-
-        return cells
+    """
+    This class tests the functionality of the GameOfLife class.
+    """
 
     def test_init(self):
-        '''
+        """
         Tests initialisation of a GameOfLife object. Also tests that
         the GameOfLife object initialises with the correct rule set.
-        '''
+
+        This method tests the initialisation of a GameOfLife object. The expected result of this test is for the
+        game engine to be correctly initialised, with the correct game engine.
+        """
         ini_gr = GolGrid()
         gol = game_of_life.GameOfLife(rule_sets.RuleSetStandard(), ini_gr)
+        # Assert the game engine has been initialised.
         assert gol
 
         rs = gol._rule_set
+        # Assert the game engine's rule set has been correctly initialised.
         assert isinstance(rs, rule_sets.RuleSetStandard)
 
     def test_set_current_generation(self):
-        '''
-        Tests that the current generation can be stored and tests
-        that the correct configuration has been stored.
-        '''
+        """
+        This method tests the ability of the game engine to set the Game of Life's current generation. The expected
+        result of this test is for the current generation to be correctly set.
+        """
         gol = game_of_life.GameOfLife(rule_sets.RuleSetStandard(), GolGrid())
 
         # Create a grid and give it a pattern of cells:
@@ -93,24 +97,23 @@ class TestGameOfLife(object):
         # adad
         gr = GolGrid(self.set_current_generation())
 
-        # Give the Game of Life this grid as the current generation
         gol._set_current_generation(gr)
 
-        # Test the current generation is not none
+        # Assert the Game of Life's current generation has been set.
         assert gol._current_generation
 
-        # Test the current generation has been given the correct pattern
         test_pattern = self.set_current_generation()
         for x, row in enumerate(gol._current_generation.get_cells()):
+            # For each row of cells.
             for y, c in enumerate(row):
+                # Assert that a cell's state is the correct state.
                 assert c.get_state() == test_pattern[x][y].get_state()
 
     def test_get_current_generation(self):
-        '''
-        Tests that the current generation can be retrieved from
-        storage and that the correct configuration has been
-        retrieved.
-        '''
+        """
+        This method tests the ability of the game engine to retrieve the Game of Life's current generation. The expected
+        result of this test is for the current generation to be correctly set.
+        """
         gol = game_of_life.GameOfLife(rule_sets.RuleSetStandard(), GolGrid())
 
         # Create a grid and give it a pattern of cells:
@@ -120,24 +123,24 @@ class TestGameOfLife(object):
         # adad
         gr = GolGrid(self.set_current_generation())
 
-        # Give the Game of Life this grid as the current generation
         gol._set_current_generation(gr)
 
-        # Test the current generation can be retrieved
         retrieved_pattern = gol.get_current_generation()
+        # Assert the current generation has been correctly retrieved.
         assert retrieved_pattern
 
-        # Test that the correct pattern has been retrieved.
         test_pattern = self.set_current_generation()
         for x, row in enumerate(retrieved_pattern.get_cells()):
+            # For each row of cells.
             for y, c in enumerate(row):
+                # Assert that a cell's state is the correct state.
                 assert c.get_state() == test_pattern[x][y].get_state()
 
     def test_set_next_generation(self):
-        '''
-        Tests that the next generation can be stored and tests
-        that the correct configuration has been stored.
-        '''
+        """
+        This method tests the ability of the game engine to set the Game of Life's next generation. The expected
+        result of this test is for the next generation to be correctly set.
+        """
         gol = game_of_life.GameOfLife(rule_sets.RuleSetStandard(), GolGrid())
 
         # Create a grid and give it a pattern of cells:
@@ -147,24 +150,23 @@ class TestGameOfLife(object):
         # dada
         gr = GolGrid(self.set_next_generation())
 
-        # Give the Game of Life this grid as the next generation
         gol._set_next_generation(gr)
 
-        # Test that the next generation is now not none
+        # Assert the that the Game of Life's next generation has been correctly set.
         assert gol._next_generation
 
-        # Test that the next generation has been set to the correct pattern
         test_pattern = self.set_next_generation()
         for x, row in enumerate(gol._next_generation.get_cells()):
+            # For each row of cells.
             for y, c in enumerate(row):
+                # Assert a cell's state is the correct state.
                 assert c.get_state() == test_pattern[x][y].get_state()
 
     def test_get_next_generation(self):
-        '''
-        Tests that the next generation can be retrieved from
-        storage and tests that the correct configuration
-        has been retrieved.
-        '''
+        """
+        This method tests the ability of the game engine to retrieve the Game of Life's next generation. The expected
+        result of this test is for the next generation to be correctly set.
+        """
         gol = game_of_life.GameOfLife(rule_sets.RuleSetStandard(), GolGrid())
 
         # Create a grid and give it a pattern of cells:
@@ -174,24 +176,24 @@ class TestGameOfLife(object):
         # dada
         gr = GolGrid(self.set_next_generation())
 
-        # Give the Game of Life this grid as the next generation
         gol._set_next_generation(gr)
 
-        # Test that the next generation can be retrieved
         retrieved_pattern = gol.get_next_generation()
+        # Assert that the Game of Life's next generation has been correctly retrieved.
         assert retrieved_pattern
 
-        # Test that the correct pattern has been retrieved.
         test_pattern = self.set_next_generation()
         for x, row in enumerate(retrieved_pattern.get_cells()):
+            # For each row of cells.
             for y, c in enumerate(row):
+                # Assert a cell's state is the correct state.
                 assert c.get_state() == test_pattern[x][y].get_state()
 
     def test_calculate_next_generation(self):
-        '''
-        Tests that the next generation of cells has been
-        correctly calculated.
-        '''
+        """
+        This method tests the ability of the game engine to calculate the next generation of the Game of Life. The
+        expected result of this test is for the next generation to be correctly calculated.
+        """
         # Create a grid and give it a pattern of cells:
         # ddad
         # aaad
@@ -211,22 +213,22 @@ class TestGameOfLife(object):
         # Give the Game of Life the first grid as the current generation
         gol = game_of_life.GameOfLife(rule_sets.RuleSetStandard(), cur_gen)
 
-        # Let the Game of Life calculate the next generation and test the
-        # result is not none.
         res = gol._calculate_next_generation(cur_gen)
+        # Assert that the game engine has calculated a new pattern.
         assert res
 
-        # Test the result against the correct result - if match, test passed
         for x, row in enumerate(res.get_cells()):
+            # For each row of cells.
             for y, c in enumerate(row):
+                # Assert that a cell has the correct state.
                 assert c.get_state() == nex_gen.get_cells()[x][y].get_state()
 
     def test_next_turn(self):
-        '''
-        Tests that the next turn of the game retrieves
-        the correct information from storage and calculates
-        the next generation of cells in the next way.
-        '''
+        """
+        This method tests the ability of the game engine to play the next turn of the Game of Life. The expected
+        result of this test is for the next turn to be played correctly, with the next generation of cells to be
+        correct.
+        """
         # Create a grid and give it a pattern of cells:
         # ddad
         # aaad
@@ -249,41 +251,43 @@ class TestGameOfLife(object):
         # Run the next turn of the game.
         gol.next_turn()
 
-        # Test that current generation has been correctly stored.
         retrieved_pattern = gol.get_current_generation()
         for x, row in enumerate(retrieved_pattern.get_cells()):
+            # For each row of cells.
             for y, c in enumerate(row):
+                # Assert that a cell has the right state.
                 assert c.get_state() == nex_gen.get_cells()[x][y].get_state()
 
     def test_get_turn_count(self):
-        '''
-        Tests the game engine's ability to retrieve the turn
-        count.
-        '''
+        """
+        This method tests the ability of the game engine to retrieve the number of turns that have been played. The
+        expeted result of this test is for the correct number of turns to be retrieved.
+        """
         cur_gen = GolGrid(self.set_current_generation())
         gol = game_of_life.GameOfLife(rule_sets.RuleSetStandard(), cur_gen)
 
         for _x in range(0, 3):
             gol.next_turn()
 
+        # After three turns, assert that the turn count is 3.
         assert gol.get_turn_count() == 3
 
     def test_is_game_forsaken(self):
-        '''
-        Tests the game engine's ability to check whether the grid
-        is completely devoid of life.
-        '''
+        """
+        This method tests the game engine's ability to evaluate the the Game of Life is over because all cells are
+        dead.
+        """
         cell_pattern = [[GolCell(Alive())]]
         cur_gen = GolGrid(cell_pattern)
 
         # Give the Game of Life the grid as the current generation
         gol = game_of_life.GameOfLife(rule_sets.RuleSetStandard(), cur_gen)
 
-        # Test grid is not forsaken.
+        # Assert that the game has not finished because not all cells are dead.
         assert not gol.is_game_forsaken()
 
         # Run next turn (cell should die)
         gol.next_turn()
 
-        # Test grid is forsaken.
+        # Assert that the game has ended because all cells are dead.
         assert gol.is_game_forsaken()
