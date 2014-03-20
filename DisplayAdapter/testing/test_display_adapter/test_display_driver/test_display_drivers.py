407a032acb307e5f936437aec4975ef69133d0c5
Michael Wilson <soothsayre@hotmail.co.uk>
2014-03-20 12:02:18 +0000

    Test functionality has been added. Paired by Richard and Michael.
    The functionality for the minutify function has now been tested; and the tests work (Support 231)

--- DisplayAdapter/testing/test_display_adapter/test_display_driver/test_display_drivers.py
+++ DisplayAdapter/testing/test_display_adapter/test_display_driver/test_display_drivers.py
@@ -4,7 +4,8 @@ the pi can correctly and sufficiently connect to the display.
 """
 
 from mock import patch
-from display_adapter.display_driver.display_drivers import DisplayDriver
+from datetime import datetime
+from display_adapter.display_driver.display_drivers import minutify, DisplayDriver
 
 class TestDisplayDriver(object):
     """
@@ -23,3 +24,15 @@ class TestDisplayDriver(object):
         assert dd
         assert hasattr(dd, "_db_helper")
         assert hasattr(dd, "_display_controller")
+
+
+def test_minutify():
+    """
+    This function tests the functionality of the minutify function linked to the Display Driver. The expected result
+    of this test is for a datetime object to be made accurate to the minute.
+    """
+
+    dt = datetime.now().replace(second=30, microsecond=40000)
+    accurate_dt = minutify(dt)
+    # Assert the datetime object has been minutified correctly (seconds and microseconds == 0)
+    assert accurate_dt.second == 0 and accurate_dt.microsecond == 0
